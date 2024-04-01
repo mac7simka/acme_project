5a89ea9b85171932317675e3422268eed5ea7c2b
Maksim Karaskin <mac7sim@gmail.com>
2024-04-01 07:23:45 +0300

    урок 2

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+# Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
+from .forms import BirthdayForm
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
+    context = {'form': form}
+    return render(request, 'birthday/birthday.html', context)
